17397fdd6ff4a9138254f3e4b511394fc5289544
DUSHYANT  JANGRA <dushyant16035@iiitd.ac.in>
2017-10-13 21:09:02 +0530

    Update GameLauncher.java

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,4 +1,7 @@
+package javaFx_projects;
+
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GameLauncher extends Application{
@@ -12,6 +15,8 @@ public class GameLauncher extends Application{
 	
 	@Override
 	public void start(Stage stage) {
+		Image image=new Image(getClass().getResourceAsStream("chainreaction.jpg"));
+		stage.getIcons().add(image);
 		sp.Start(stage);
 		
 	}
